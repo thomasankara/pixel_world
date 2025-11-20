2efb1d6806cc3925e865d117f15a5da0649ceb59
thomasankara <156187785+thomasankara@users.noreply.github.com>
2025-11-20 10:31:48 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,35 +1,29 @@
-# Fichier: server.py
 import socketio
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse  # <--- AJOUT 1
 
-# Création du serveur
 sio = socketio.AsyncServer(async_mode='asgi', cors_allowed_origins='*')
 app = FastAPI()
 
-# Gestion des fichiers statiques (HTML/JS/CSS)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 socket_app = socketio.ASGIApp(sio, app)
 
-# -- État du monde (Mémoire vive) --
-# On stocke juste une liste de points pour commencer
 world_state = []
+
+# --- AJOUT 2 : La redirection automatique ---
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/static/index.html")
 
 @sio.event
 async def connect(sid, environ):
     print(f"Joueur connecté : {sid}")
-    # On envoie l'état actuel au nouveau joueur uniquement
     await sio.emit('init_world', world_state, to=sid)
 
 @sio.event
 async def place_circle(sid, data):
-    # data = {'x': int, 'y': int, 'color': str}
-    print(f"Cercle placé : {data}")
-    
-    # 1. Sauvegarder
     world_state.append(data)
-    
-    # 2. Diffuser à TOUS les joueurs connectés
     await sio.emit('new_circle', data)
 
 @sio.event
